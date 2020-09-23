c3ca06a704efe64e33f3abf421c77e8d660f6f83
GdoongMathew <dungdudu426@gmail.com>
2020-09-24 02:35:52 +0800

    fix not removing items from que_list.

--- Distributor.py
+++ Distributor.py
@@ -14,23 +14,23 @@ def all_to_avg(l1: List, d1: Dict[Any, List]) -> Dict:
     assert isinstance(l1, (list, tuple)), 'l1 should be a list or tuple.'
     assert isinstance(d1, dict), 'd1 should ba a dictionary.'
 
-    d_ori = d1.copy()
+    d_ori = dict(sorted(d1.items(), key=lambda x: -len(x[1])))
     d_count = sum([len(x) for x in d1.values()])
     it_num = d_count // len(l1)
     remainder = d_count % len(l1)
 
     que_list = []
     for k, v in d_ori.items():
+        v_len = len(v)
         if k not in l1:
             que_list.extend(v)
             d1.pop(k)
-        elif len(v) > it_num:
+        elif v_len > it_num:
             d1[k] = v[:it_num]
             que_list.extend(v[it_num:])
-        else:
-            d1[k].extend(que_list[:it_num - len(v)])
-            que_list = que_list[it_num - len(v):]
-
+        elif v_len < it_num:
+            d1[k].extend(que_list[:it_num - v_len])
+            que_list = que_list[it_num - v_len:]
     return d1
 
 
@@ -44,7 +44,8 @@ if __name__ == '__main__':
             d1[v] = [k]
         else:
             d1[v].append(k)
-    d2 = all_to_avg(l1, d1)
+    d2 = all_to_avg(l1, d1.copy())
+    print(d1)
     print(d2)
 
 
